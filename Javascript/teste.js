a212d791be8625aad5a9a77ee8b0c23839902fd1
hugoluiz1984 <57529085+hugoluiz1984@users.noreply.github.com>
2022-06-24 10:25:12 -0300

    novo teste

--- Javascript/teste.js
+++ Javascript/teste.js
@@ -1,2 +1,13 @@
 /*Teste*/
 // comentário em uma linha
+let arr =[1];
+for (let i = 1; i <=9; i++){ //criar array para os fatoriais com 
+    let fatorial = 1
+    for (let j = i; j>=1;j--){
+        fatorial=fatorial*j;
+        console.log(fatorial,j,i)
+    }
+    arr.push(fatorial)
+}
+console.log(arr)
+
